28ee146724cef06f93f01143af45c0cc3662e8eb
Austin New <literalsands@gmail.com>
2017-09-29 21:03:37 -0400

    removed empty server files. added basic documentation to client scripts.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,10 @@
+/**
+ * This is the root file for the Meteor client-side application.
+ */
+
+// Imports react router setup and applications.
 import '/imports/startup/client';
+// Imports required for react-mdl to work properly.
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+
